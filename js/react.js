b37f8211cfc5fa2945dead81735dd80ccc8dcdb7
Rafael Garcia <1337technomage@gmail.com>
2016-05-21 23:12:41 -0400

    Put a template in index.html. Gonna flesh out the site with foundation and then use react and javascript later.

---
+++ js/react.js
@@ -0,0 +1,10 @@
+/**
+ * Created by tetrix on 5/20/16.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+<h1>Hello, world!</h1>,
+    document.getElementById('example')
+);
