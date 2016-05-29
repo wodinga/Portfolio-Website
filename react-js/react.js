ef571f6696a65f42d87ef96531a4bfb1b9a42444
Rafael Garcia <1337technomage@gmail.com>
2016-05-29 13:33:11 -0400

    While working on the react-bootstrap branch I realized I need to use className instead of class to set attributes.

--- react-js/react.js
+++ react-js/react.js
@@ -10,44 +10,44 @@ var App = React.createClass({
     render: function() {
         return (
             <div className="app">
-                <div class="off-canvas-wrapper">
-                    <div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
-                        <div class="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas data-position="left">
-                            <div class="row column">
+                <div className="off-canvas-wrapper">
+                    <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+                        <div className="off-canvas position-left reveal-for-large" id="my-info" data-off-canvas data-position="left">
+                            <div className="row column">
                                 <br />
-                                    <img class="thumbnail" src="http://placehold.it/550x350" />
+                                    <img className="thumbnail" src="http://placehold.it/550x350" />
                                         <h5>Mike Mikerson</h5>
                                         <p>Duis aliquet egestas purus in blandit. Curabitur vulputate, ligula lacinia scelerisque tempor, lacus lacus ornare ante, ac egestas est urna sit amet arcu. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed molestie augue sit amet leo.</p>
                             </div>
                         </div>
-                        <div class="off-canvas-content" data-off-canvas-content>
-                            <div class="title-bar hide-for-large">
-                                <div class="title-bar-left">
-                                    <button class="menu-icon" type="button" data-open="my-info"></button>
-                                    <span class="title-bar-title">Mike Mikerson</span>
+                        <div className="off-canvas-content" data-off-canvas-content>
+                            <div className="title-bar hide-for-large">
+                                <div className="title-bar-left">
+                                    <button className="menu-icon" type="button" data-open="my-info"></button>
+                                    <span className="title-bar-title">Mike Mikerson</span>
                                 </div>
                             </div>
-                            <div class="callout primary">
-                                <div class="row column">
+                            <div className="callout primary">
+                                <div className="row column">
                                     <h1>Hello! This is the portfolio of a very witty person.</h1>
-                                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla.</p>
+                                    <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla.</p>
                                 </div>
                             </div>
-                            <div id="Gallery" class="row small-up-2 medium-up-3 large-up-4">
+                            <div id="Gallery" className="row small-up-2 medium-up-3 large-up-4">
 
                             </div>
                             <hr />
-                                <div class="row">
-                                    <div class="medium-6 columns">
+                                <div className="row">
+                                    <div className="medium-6 columns">
                                         <h3>Contact Me</h3>
                                         <p>Vivamus hendrerit arcu sed erat molestie vehicula. Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor. Ut in nulla enim. Phasellus molestie magna non est bibendum non venenatis nisl tempor. Suspendisse dictum feugiat nisl ut dapibus. Mauris iaculis porttitor.</p>
-                                        <ul class="menu">
+                                        <ul className="menu">
                                             <li><a href="#">Dribbble</a></li>
                                             <li><a href="#">Facebook</a></li>
                                             <li><a href="#">Yo</a></li>
                                         </ul>
                                     </div>
-                                    <div class="medium-6 columns">
+                                    <div className="medium-6 columns">
                                         <label>Name
                                             <input type="text" placeholder="Name" />
                                         </label>
@@ -58,7 +58,7 @@ var App = React.createClass({
                                             Message
                                             <textarea placeholder="holla at a designerd"></textarea>
                                         </label>
-                                        <input type="submit" class="button expanded" value="Submit" />
+                                        <input type="submit" className="button expanded" value="Submit" />
                                     </div>
                                 </div>
                         </div>
